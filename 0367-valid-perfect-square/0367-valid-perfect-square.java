0983d18391f3e4501603947e36e109eec3045f21
Yashwanth E <yyashwanth293@gmail.com>
2026-09-22 19:41:16 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.41%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+    int low=0;
+    int high=num;
+    while(low<=high){
+        int mid= low+(high-low)/2;
+
+        if((long)mid*mid==num){
+            return true;
+        }
+        if((long)mid*mid<num){
+            low=mid+1;
+        }
+       else{
+            high=mid-1;
+        }
+        
+    }
+    return false;
+    }
+}
